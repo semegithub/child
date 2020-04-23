15ea73cd38248bcd2c0949b9d7a2386c84abdc0b
emery <emery@localhost.localdomain>
2020-04-23 19:47:15 +0200

    repo

--- src/main/java/com/openshift/cloudnative/poc/autoscaling/child/MyRepository.java
+++ src/main/java/com/openshift/cloudnative/poc/autoscaling/child/MyRepository.java
@@ -5,8 +5,15 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 
-public interface MyRepository extends CrudRepository<MyEntity, String> {
+public interface MyRepository extends CrudRepository<MyEntity, Integer> {
 
     List<MyEntity> findByName(String f1);
-
+    
+    MyEntity findByEntityId(int f1);
+    
+    MyEntity save(MyEntity entity);
+    
+    void delete(MyEntity entity);
+    
+    
 }
