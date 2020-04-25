c1080aab749476566ba46923faaa5506445ffae3
emery <emery@localhost.localdomain>
2020-04-25 17:53:47 +0200

    test

--- src/main/java/com/openshift/cloudnative/poc/autoscaling/child/MyRepository.java
+++ src/main/java/com/openshift/cloudnative/poc/autoscaling/child/MyRepository.java
@@ -1,16 +1,17 @@
 package com.openshift.cloudnative.poc.autoscaling.child;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MyRepository extends PagingAndSortingRepository<MyEntity, Integer> {
+public interface MyRepository extends JpaRepository<MyEntity, Integer> {
 
 //    List<MyEntity> findByF1(String f1);
     
     Optional<MyEntity> findById(int id);
-    Iterable<MyEntity> findAll();
+    List<MyEntity> findAll();
     
     Iterable<MyEntity> findAll(PageRequest pageRequest);
     
