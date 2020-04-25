ea76070f76331aff5da200bec3690081413b4b3b
emery <emery@localhost.localdomain>
2020-04-25 19:45:22 +0200

    jpa

--- src/main/java/com/openshift/cloudnative/poc/autoscaling/child/ChildApplication.java
+++ src/main/java/com/openshift/cloudnative/poc/autoscaling/child/ChildApplication.java
@@ -3,9 +3,11 @@ package com.openshift.cloudnative.poc.autoscaling.child;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @EnableJpaRepositories
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class ChildApplication {
 
 	public static void main(String[] args) {
